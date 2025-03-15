254789cd42f5a671b05d71cd96c518d8bd65b97e
Korir Carlos <carlkorir226@gmail.com>
2025-03-15 20:38:57 +0300

    adds and styles h1

---
+++ index.js
@@ -0,0 +1,10 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+ ];
+ const first = document.getElementById('top')
+console.log(first)
+first.style.color = 'white'
+first.style.backgroundColor = 'black'
+first.style.textAlign = 'center'
